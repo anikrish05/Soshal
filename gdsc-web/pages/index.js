4945ea411ff7599a93d85798137167e45d496eb5
Anirudh Krishna <kris.anirudh@gmail.com>
2023-11-06 12:30:56 -0800

    added basic google maps api

--- gdsc-web/pages/index.js
+++ gdsc-web/pages/index.js
@@ -1,10 +1,34 @@
-import Head from 'next/head';
+import { useLoadScript, GoogleMap } from '@react-google-maps/api';
+import { useMemo } from 'react';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+const Home = () => {
+  const libraries = useMemo(() => ['places'], []);
+  const mapCenter = useMemo(
+    () => ({ lat: 36.9905, lng: -122.0584 }),
+    []
+  );
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY,
+    libraries: libraries,
+  });
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div className={styles.container}>
-       <h1>GDSC</h1>
+    <div className={styles.homeWrapper}>
+      <GoogleMap
+        zoom={15}
+        center={mapCenter}
+        mapTypeId={google.maps.MapTypeId.ROADMAP}
+        mapContainerStyle={{ width: '800px', height: '800px' }}
+        onLoad={() => console.log('Map Component Loaded...')}
+      />
     </div>
   );
-}
+};
+
+export default Home;
