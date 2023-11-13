1e8235134c5cc2f4b4ff7491985d01364bb8ec02
Anirudh Krishna <kris.anirudh@gmail.com>
2023-11-12 17:04:03 -0800

    changed server to express

--- gdsc_server/server.js
+++ gdsc_server/server.js
@@ -1,20 +1,13 @@
-const path = "localhost:3000/"
-var http = require('http'); // Import Node.js core module
-//const firebase_app = require("lib/firebase.js");
-var server = http.createServer(function (req, res) {   //create web server
-    if (req.url == '/') { //check the URL of the current request
-        
-        // set response header
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.write(JSON.stringify({ message: "Hello World"}));  
-            res.end(); 
-    
-    }
-    else
-        res.end('Invalid Request!');
+const express = require('express')
+const app = express()
 
-});
+const firebaseApp = initializeApp(firebaseConfig);
+const port = 3000
 
-server.listen(3000); //6 - listen for any incoming requests
+app.get('/', (req, res) => {
+  res.send(JSON.stringify({ message: "Helljo World"}))
+})
 
-console.log('Node.js web server at port 3000 is running..')
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
