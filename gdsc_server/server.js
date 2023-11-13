54d2d039c712650b1c8dd2f04b63e5241658076b
Anirudh Krishna <kris.anirudh@gmail.com>
2023-11-12 16:08:26 -0800

    small changes

--- gdsc_server/server.js
+++ gdsc_server/server.js
@@ -10,20 +10,6 @@ var server = http.createServer(function (req, res) {   //create web server
             res.end(); 
     
     }
-    else if (req.url == "/student") {
-        
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is student Page.</p></body></html>');
-        res.end();
-    
-    }
-    else if (req.url == "/admin") {
-        
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is admin Page.</p></body></html>');
-        res.end();
-    
-    }
     else
         res.end('Invalid Request!');
 
