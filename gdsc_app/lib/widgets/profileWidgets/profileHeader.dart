2b3f1b3ba01e91e369e19c77f9501f4e89c7a097
AdithyaPradeep05 <adithyaapradeep@gmail.com>
2023-11-27 17:35:43 -0800

    profileHeader change

--- gdsc_app/lib/widgets/profileWidgets/profileHeader.dart
+++ gdsc_app/lib/widgets/profileWidgets/profileHeader.dart
@@ -2,21 +2,37 @@ import 'package:flutter/material.dart';
 
 class ProfileHeaderWidget extends StatefulWidget {
   late String _image;
+  late String _name; // Added _name
+  late int _graduationYear; // Added _graduationYear
   late VoidCallback _onClicked;
-  ProfileHeaderWidget(String image, VoidCallback onClicked){
+
+  ProfileHeaderWidget(String image, VoidCallback onClicked) {
     this._image = image;
     this._onClicked = onClicked;
   }
+
+  void setProfileInfo(String name, int graduationYear) {
+    _name = name;
+    _graduationYear = graduationYear;
+  }
+
   @override
-  _ProfileHeaderWidgetState createState() => _ProfileHeaderWidgetState(_image, _onClicked);
+  _ProfileHeaderWidgetState createState() =>
+      _ProfileHeaderWidgetState(_image, _name, _graduationYear, _onClicked);
 }
 
 class _ProfileHeaderWidgetState extends State<ProfileHeaderWidget> {
   late String _image;
+  late String _name;
+  late int _graduationYear;
   late VoidCallback _onClicked;
-  _ProfileHeaderWidgetState(String _image, VoidCallback _onClicked){
-    this._image = _image;
-    this._onClicked = _onClicked;
+
+  _ProfileHeaderWidgetState(
+      String image, String name, int graduationYear, VoidCallback onClicked) {
+    this._image = image;
+    this._name = name;
+    this._graduationYear = graduationYear;
+    this._onClicked = onClicked;
   }
 
   @override
@@ -27,32 +43,26 @@ class _ProfileHeaderWidgetState extends State<ProfileHeaderWidget> {
           child: Material(
             color: Colors.transparent,
             child: Ink.image(
-                image: AssetImage(_image),
+              image: AssetImage(_image),
               fit: BoxFit.cover,
               width: 128,
               height: 128,
               child: InkWell(onTap: _onClicked),
             ),
-          )
+          ),
         ),
         Positioned(
           bottom: 0,
-            right: 0,
-            child: buildEditIcon(Colors.orange)),
-      ]
+          right: 0,
+          child: buildEditIcon(Colors.orange),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          child: buildProfileInfo(),
+        ),
+      ],
     );
-    return Scaffold(
-      child: Container(
-        padding: const EdgeInsets.all(tDefaultSize),
-        child: Column(
-          children: [
-            SizedBox(
-              width: 120, height: 120,
-            )
-          ]
-        )
-      )
-    )
   }
 
   Widget buildEditIcon(Color color) => buildCircle(
@@ -60,8 +70,8 @@ class _ProfileHeaderWidgetState extends State<ProfileHeaderWidget> {
     all: 3,
     child: buildCircle(
       color: color,
-      all:8,
-      child:   Icon(
+      all: 8,
+      child: Icon(
         Icons.edit,
         color: Colors.white,
         size: 20,
@@ -69,17 +79,50 @@ class _ProfileHeaderWidgetState extends State<ProfileHeaderWidget> {
     ),
   );
   Widget buildCircle({
-  required Widget child,
+    required Widget child,
     required double all,
-    required Color color
-}) =>ClipOval(
-    child:   Container(
-      padding: EdgeInsets.all(all),
-      color: color,
-      child: child
-    ),
-);
+    required Color color,
+  }) =>
+      ClipOval(
+        child: Container(
+          padding: EdgeInsets.all(all),
+          color: color,
+          child: child,
+        ),
+      );
 
+  Widget buildProfileInfo() {
+    return Container(
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Colors.black.withOpacity(0.5),
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(10),
+          bottomRight: Radius.circular(10),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            _name,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 4),
+          Text(
+            'Class of $_graduationYear',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
 
 }
