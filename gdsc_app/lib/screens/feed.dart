19f1231003a8cafb0bb3d918825031b75687769b
AdithyaPradeep05 <adithyaapradeep@gmail.com>
2023-12-07 16:29:55 -0800

    made sliding pannel work

--- gdsc_app/lib/screens/feed.dart
+++ gdsc_app/lib/screens/feed.dart
@@ -2,7 +2,8 @@ import 'dart:async';
 import 'package:custom_info_window/custom_info_window.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import '../widgets/slidingUpWidget.dart';
+import 'package:gdsc_app/widgets/slidingUpWidget.dart';
+
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class MyApp extends StatefulWidget {
@@ -12,13 +13,10 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   Completer<GoogleMapController> _controller = Completer();
-  CustomInfoWindowController _customInfoWindowController = CustomInfoWindowController();
+  PanelController panelController = PanelController();
   final List<Marker> _markers = <Marker>[];
 
   final LatLng _center = const LatLng(36.9907207008804, -122.05845686120782);
-
-
-  //bool isSlidingPanelVisible = false;
 
   @override
   void initState() {
@@ -26,21 +24,18 @@ class _MyAppState extends State<MyApp> {
     loadData();
   }
 
-  void loadData (){
+  void loadData() {
     _markers.add(
       Marker(
         markerId: MarkerId('UCSC'),
         position: LatLng(36.9907207008804, -122.05845686120782),
         onTap: () {
-          _customInfoWindowController.addInfoWindow!(
-              SlidingUpPanel(
-                panel: Center(child: Text("This is the sliding Widget"),),
-              ),
-              LatLng(36.9907207008804, -122.05845686120782)
-          );
-        }
-      //infoWindow: InfoWindow(title: 'UCSC'),
-    )
+          // Show/hide the sliding panel when marker is tapped
+          panelController.isPanelOpen
+              ? panelController.close()
+              : panelController.open();
+        },
+      ),
     );
   }
 
@@ -54,71 +49,28 @@ class _MyAppState extends State<MyApp> {
     controller.dispose();
   }
 
-  /*
-  void handleTap(LatLng latLng) {
-    print("in handle tap");
-    Marker tappedMarker = _markers.firstWhere(
-          (marker) => marker.position == latLng,
-      //orElse: () => Marker(markerId: MarkerId('dummy')),
-    );
-    print(tappedMarker);
-    if (tappedMarker.markerId != MarkerId('dummy')) {
-      print("in marker pressed");
-      showMarkerInfo(context, tappedMarker);
-      setState(() {
-        isSlidingPanelVisible = true;
-      });
-    }
-  }
-*/
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Stack(
-          children: <Widget>[
-            GoogleMap(
-              onMapCreated: (GoogleMapController controller) {
-                _customInfoWindowController.googleMapController = controller;
-              },
-              markers: Set<Marker>.of(_markers),
-              initialCameraPosition: CameraPosition(
-                target: _center,
-                zoom: 16.0,
-              ),
-              onTap: (position) {
-                _customInfoWindowController.hideInfoWindow!();
-              },
-              onCameraMove: (position){
-                _customInfoWindowController.onCameraMove!();
-              }
+        body: SlidingUpPanel(
+          controller: panelController,
+          minHeight: 0,
+          maxHeight: 300.0, // Adjust as needed
+          panel: SlidingUpWidget(),
+          body: GoogleMap(
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+            markers: Set<Marker>.of(_markers),
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 16.0,
             ),
-            CustomInfoWindow(
-              controller: _customInfoWindowController,
-            ),
-       ],
+          ),
         ),
       ),
     );
   }
-
-  void showMarkerInfo(BuildContext context, Marker marker) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(marker.infoWindow.title ?? 'No Title'),
-          content: Text('You tapped on the marker!'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Close'),
-            ),
-          ],
-        );
-      },
-    );
-  }
 }
+
--- gdsc_app/lib/widgets/slidingUpWidget.dart
+++ gdsc_app/lib/widgets/slidingUpWidget.dart
@@ -1,51 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
-
-class SlidingUpWidget extends StatefulWidget {
-  final bool isSlidingPanelVisible;
-
-  const SlidingUpWidget({required this.isSlidingPanelVisible});
-
-  @override
-  _SlidingUpWidgetState createState() => _SlidingUpWidgetState();
-}
-
-class _SlidingUpWidgetState extends State<SlidingUpWidget> {
-  double _panelHeightClosed = 0.0;
-  double _panelHeightOpened = 200.0;
-
+class SlidingUpWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SlidingUpPanel(
-      panel: _buildPanelContent(),
-      minHeight: _panelHeightClosed,
-      maxHeight: _panelHeightOpened,
-      body: Container(), // Empty container to prevent interference with the underlying map
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Hi',
+            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 16.0),
+          Text(
+            'This is the sliding panel content.',
+            style: TextStyle(fontSize: 16.0),
+          ),
+        ],
+      ),
     );
-  }
-
-  Widget _buildPanelContent() {
-    if (widget.isSlidingPanelVisible) {
-      return Container(
-        padding: EdgeInsets.all(16.0),
-        color: Colors.white,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Hi',
-              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'This is the sliding panel content.',
-              style: TextStyle(fontSize: 16.0),
-            ),
-          ],
-        ),
-      );
-    } else {
-      return Container(); // Empty container if the panel is not visible
-    }
   }
 }
