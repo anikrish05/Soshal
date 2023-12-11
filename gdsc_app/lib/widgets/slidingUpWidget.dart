0211478b7b6ff5a0ffbecd5e92540c14c15d4ac3
AdithyaPradeep05 <adithyaapradeep@gmail.com>
2023-12-10 17:33:52 -0800

    scroll feature

--- gdsc_app/lib/widgets/slidingUpWidget.dart
+++ gdsc_app/lib/widgets/slidingUpWidget.dart
@@ -14,9 +14,14 @@ class SlidingUpWidget extends StatefulWidget {
 class _SlidingUpWidgetState extends State<SlidingUpWidget> {
   // List to store comments
   List<Comment> comments = [
-    Comment(username: 'Kamble', text: 'Let\'s get lit'),
-    Comment(username: 'User', text: 'This isn\'t lowkey the vibe'),
-    Comment(username: 'AnotherUser', text: 'Nice event!'),
+    Comment(username: 'Kamble', text: 'Let\'s get lit!'),
+    Comment(username: 'Adithya', text: 'Can\'t miss this opportunity to see her!?'),
+    Comment(username: 'Venkat', text: 'I am coming out!!!!!'),
+    Comment(username: 'Venkat', text: 'I am coming out!!!!!'),
+    Comment(username: 'Venkat', text: 'I am coming out!!!!!'),
+    Comment(username: 'Venkat', text: 'I am coming out!!!!!'),
+    Comment(username: 'Venkat', text: 'I am coming out!!!!!'),
+    Comment(username: 'Venkat', text: 'I am coming out!!!!!'),
   ];
 
   // Controller for the comment text field
@@ -26,184 +31,192 @@ class _SlidingUpWidgetState extends State<SlidingUpWidget> {
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.center,
-      child: GestureDetector(
-        onTap: () {
-          // Close the panel and call the onClose callback when tapped outside the panel
-          widget.onClose();
-        },
-        child: Container(
-          width: MediaQuery.of(context).size.width * 1.0,
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: 12),
-                child: Container(
-                  height: 4,
-                  width: 100,
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+      child: Container(
+        width: MediaQuery.of(context).size.width * 1.0,
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 12),
+              child: Container(
+                height: 4,
+                width: 100,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(left: 12),
-                      width: 125,
-                      height: 125,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.network(
-                          widget.markerData.image,
-                          fit: BoxFit.cover,
-                        ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 12),
+                    width: 125,
+                    height: 125,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        widget.markerData.image,
+                        fit: BoxFit.cover,
                       ),
                     ),
-                    SizedBox(width: 16),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(height: 10),
-                          Row(
-                            children: [
-                              Text(
-                                widget.markerData.title,
-                                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 5),
-                          Row(
-                            children: [
-                              Text(
-                                widget.markerData.description,
-                                style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 8),
-                          Row(
-                            children: [
-                              Text("By: Adithya "),
-                              SizedBox(width: 8),
-                              Row(
-                                children: List.generate(
-                                  5,
-                                      (index) => Padding(
-                                    padding: EdgeInsets.only(right: 4),
-                                    child: Icon(Icons.star, color: Colors.grey, size: 16),
-                                  ),
+                  ),
+                  SizedBox(width: 16),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Text(
+                              widget.markerData.title,
+                              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 5),
+                        Row(
+                          children: [
+                            Text(
+                              widget.markerData.description,
+                              style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 8),
+                        Row(
+                          children: [
+                            Text("By: Adithya "),
+                            SizedBox(width: 8),
+                            Row(
+                              children: List.generate(
+                                5,
+                                    (index) => Padding(
+                                  padding: EdgeInsets.only(right: 4),
+                                  child: Icon(Icons.star, color: Colors.grey, size: 16),
                                 ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 8),
-                          Row(
-                            children: [
-                              Icon(Icons.location_on),
-                              Padding(padding: EdgeInsets.only(right: 4)),
-                              Text(widget.markerData.location),
-                            ],
-                          ),
-                          SizedBox(height: 8),
-                          Row(
-                            children: [
-                              Icon(Icons.access_time),
-                              Padding(padding: EdgeInsets.only(right: 4)),
-                              Text(widget.markerData.time),
-                            ],
-                          ),
-                          SizedBox(height: 8),
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child: Container(
-                              margin: EdgeInsets.only(bottom: 2, right: 40),
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  // Add RSVP button logic
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  primary: Colors.grey,
-                                  textStyle: TextStyle(
-                                    fontFamily: 'Borel',
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
-                                ),
-                                child: Text('rsvp'),
                               ),
                             ),
+                          ],
+                        ),
+                        SizedBox(height: 8),
+                        Row(
+                          children: [
+                            Icon(Icons.location_on),
+                            Padding(padding: EdgeInsets.only(right: 4)),
+                            Text(widget.markerData.location),
+                          ],
+                        ),
+                        SizedBox(height: 8),
+                        Row(
+                          children: [
+                            Icon(Icons.access_time),
+                            Padding(padding: EdgeInsets.only(right: 4)),
+                            Text(widget.markerData.time),
+                          ],
+                        ),
+                        SizedBox(height: 8),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Container(
+                            margin: EdgeInsets.only(bottom: 2, right: 40),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                // Add RSVP button logic
+                              },
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.grey,
+                                textStyle: TextStyle(
+                                  fontFamily: 'Borel',
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                              child: Text('rsvp'),
+                            ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              Divider(
-                color: Colors.grey,
-                thickness: 1,
-                indent: 50,
-                endIndent: 50,
-              ),
-              // Text field for adding comments
-              Padding(
-                padding: const EdgeInsets.only(right: 40, left: 40),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Container(
-                    height: 40,
-                    child: TextField(
-                      controller: commentController,
-                      style: TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        hintText: 'add comments',
-                        filled: true,
-                        fillColor: Colors.grey,
-                        hintStyle: TextStyle(
-                          fontFamily: 'Borel',
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                        contentPadding: EdgeInsets.symmetric(vertical: 1, horizontal: 10),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.transparent),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.transparent),
-                        ),
-                        border: InputBorder.none,
-                        suffixIcon: IconButton(
-                          onPressed: () {
-                            addComment(); // Function to add the comment
-                          },
-                          icon: Icon(Icons.send, color: Colors.white),
-                        ),
+            ),
+            Divider(
+              color: Colors.grey,
+              thickness: 1,
+              indent: 50,
+              endIndent: 50,
+            ),
+            // Text field for adding comments
+            Padding(
+              padding: const EdgeInsets.only(right: 40, left: 40),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 40,
+                  child: TextField(
+                    controller: commentController,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText: 'add comments',
+                      filled: true,
+                      fillColor: Colors.grey,
+                      hintStyle: TextStyle(
+                        fontFamily: 'Borel',
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                      contentPadding: EdgeInsets.symmetric(vertical: 1, horizontal: 10),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.transparent),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.transparent),
+                      ),
+                      border: InputBorder.none,
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          addComment(); // Function to add the comment
+                        },
+                        icon: Icon(Icons.send, color: Colors.white),
                       ),
                     ),
                   ),
                 ),
               ),
-              SizedBox(height: 16), // Add padding under the text box
-              // Comment section
-              Expanded(
-                child: ListView.builder(
-                  itemCount: comments.length,
-                  itemBuilder: (context, index) {
-                    return buildCommentItem(comments[index]);
-                  },
+            ),
+            SizedBox(height: 16), // Add padding under the text box
+            // Comment section with scrollbar
+            Expanded(
+              child: Container(
+                height: 200, // Set a fixed height for the comment section
+                child: Scrollbar(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxHeight: 200,
+                      ),
+                      child: ListView.builder(
+                        reverse: true, // Set reverse to true
+                        itemCount: comments.length,
+                        itemBuilder: (context, index) {
+                          return buildCommentItem(comments[index]);
+                        },
+                      ),
+                    ),
+                  ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
@@ -219,12 +232,6 @@ class _SlidingUpWidgetState extends State<SlidingUpWidget> {
               '${comment.username}: ${comment.text}',
               style: TextStyle(color: Colors.grey),
             ),
-          ),
-          IconButton(
-            onPressed: () {
-              // Add functionality for the heart-shaped button
-            },
-            icon: Icon(Icons.favorite_border, color: Colors.grey),
           ),
         ],
       ),
