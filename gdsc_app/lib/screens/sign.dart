2fde0e210a21e0cedf87592521772932bf0f6053
Anirudh Krishna <kris.anirudh@gmail.com>
2023-12-20 19:07:19 -0800

    verification done for singup

--- gdsc_app/lib/screens/sign.dart
+++ gdsc_app/lib/screens/sign.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:gdsc_app/screens/profile.dart';
 import 'package:http/http.dart';
+
 class SignUpScreen extends StatefulWidget {
   @override
   _SignUpScreenState createState() => _SignUpScreenState();
@@ -16,14 +18,27 @@ class _SignUpScreenState extends State<SignUpScreen> {
   Color _color1 = Color(0xFFFF8050);
   Color _color2 = Color(0xFFF0F0F0);
 
+  String errorMessage = '';
+
   bool isValidEmail(input) {
     return RegExp(
         r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
         .hasMatch(input);
   }
-  void createAccount() async{
-    print(isChecked);
-    final response = await post(Uri.parse('http://10.0.2.2:3000/signup'),
+
+  void createAccount() async {
+    // Check if the email is a valid UCSC email
+    if (!isValidEmail(emailController.text)) {
+      // Show an error message for an invalid email
+      setState(() {
+        errorMessage = 'Invalid email. Please use a UCSC email.';
+      });
+      return;
+    }
+
+    // Perform the account creation logic
+    final response = await post(
+      Uri.parse('http://10.0.2.2:3000/api/users/signup'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -34,14 +49,38 @@ class _SignUpScreenState extends State<SignUpScreen> {
         "isOwner": isChecked ? "true" : "false"
       }),
     );
-    print(response.statusCode);
-    if(response.statusCode == 200){
-      if(isChecked){
-        Navigator.pushNamed(context, '/createClub');
+
+    if (response.statusCode == 200) {
+      if (isChecked) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ProfileScreen(),
+          ),
+        );
       }
       Navigator.pushNamed(context, '/home');
+    } else {
+      // Handle other error cases
+      final Map<String, dynamic> responseData = jsonDecode(response.body);
+
+      if (responseData.containsKey('error') &&
+          responseData['error']['code'] == 'auth/invalid-email') {
+        // Show an error message for invalid email from Firebase
+        setState(() {
+          errorMessage = 'Invalid email. Please check your email address.';
+        });
+      } else {
+        // Show a generic error message for other errors
+        setState(() {
+          errorMessage = 'An error occurred. Please try again later.';
+        });
+      }
     }
   }
+
+
+
   @override
   Widget build(BuildContext context) {
     Color getColor(Set<MaterialState> states) {
@@ -55,80 +94,75 @@ class _SignUpScreenState extends State<SignUpScreen> {
       }
       return Colors.white;
     }
+
     return Scaffold(
-        body: Center(
-          child: ListView(
-            shrinkWrap: true,
-            children: <Widget>[
-              Image.asset('assets/image.png',
-                  height: 100
-              ),
-              Padding(
-                  padding: EdgeInsets.only(bottom: 90)
-              ),
-              buildNameWidget(),
-
-              Padding(
-                  padding: EdgeInsets.only(bottom: 8)
-              ),
-              buildEmailWidget(),
-              Padding(
-                  padding: EdgeInsets.only(bottom: 8)
-              ),
-              buildPasswordWidget(),
-
-              Padding(
-                  padding: EdgeInsets.only(bottom: 8)
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Are you a club owner?',
-                      style: TextStyle(
-                          fontSize: 10,
-                          letterSpacing: 2.0,
-                          color: Colors.black,
-                          fontFamily: 'Borel'
-                      )
-                  ),
-                  Checkbox(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(2.0),
-                    ),
-                    side: MaterialStateBorderSide.resolveWith(
-                          (states) => BorderSide(width: 1.0, color: Colors.black),
-                    ),
-                    checkColor: Colors.black,
-                    fillColor: MaterialStateProperty.resolveWith(getColor),
-                    value: isChecked,
-                    onChanged: (bool? value){
-                      setState(() {
-                        isChecked = value!;
-                      });
-                    },
-                  ),
-                ],
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.blue,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            Image.asset('assets/image.png', height: 100),
+            Padding(padding: EdgeInsets.only(bottom: 90)),
+            // Display error message
+            if (errorMessage.isNotEmpty)
+              Center(
+                child: Text(
+                  errorMessage,
+                  style: TextStyle(color: Colors.red),
                 ),
-                onPressed: () {createAccount(); },
-                child: Text('Sign up',
+              ),
+            buildNameWidget(),
+            Padding(padding: EdgeInsets.only(bottom: 8)),
+            buildEmailWidget(),
+            Padding(padding: EdgeInsets.only(bottom: 8)),
+            buildPasswordWidget(),
+            Padding(padding: EdgeInsets.only(bottom: 8)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Are you a club owner?',
                     style: TextStyle(
-                        fontFamily: "borel",
-                        decoration: TextDecoration.underline,
+                        fontSize: 10,
+                        letterSpacing: 2.0,
                         color: Colors.black,
-                      fontSize: 18.3
-                    )
+                        fontFamily: 'Borel')),
+                Checkbox(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(2.0),
+                  ),
+                  side: MaterialStateBorderSide.resolveWith(
+                          (states) => BorderSide(width: 1.0, color: Colors.black)),
+                  checkColor: Colors.black,
+                  fillColor: MaterialStateProperty.resolveWith(getColor),
+                  value: isChecked,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      isChecked = value!;
+                    });
+                  },
                 ),
+              ],
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.blue,
               ),
-            ],
-          ),
-        )
+              onPressed: () {
+                createAccount();
+              },
+              child: Text('Sign up',
+                  style: TextStyle(
+                      fontFamily: "borel",
+                      decoration: TextDecoration.underline,
+                      color: Colors.black,
+                      fontSize: 18.3)),
+            ),
+          ],
+        ),
+      ),
     );
   }
-  Widget buildNameWidget(){
+
+  Widget buildNameWidget() {
     return SizedBox(
       width: 350,
       child: TextFormField(
@@ -143,12 +177,11 @@ class _SignUpScreenState extends State<SignUpScreen> {
             contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
             filled: true,
             fillColor: _color2,
-          )
-      ),
+          )),
     );
   }
 
-  Widget buildEmailWidget(){
+  Widget buildEmailWidget() {
     return SizedBox(
       width: 350,
       child: TextFormField(
@@ -163,12 +196,12 @@ class _SignUpScreenState extends State<SignUpScreen> {
             contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
             filled: true,
             fillColor: _color2,
-          )
-      ),
+          )),
     );
   }
-  Widget buildPasswordWidget(){
-    return  SizedBox(
+
+  Widget buildPasswordWidget() {
+    return SizedBox(
       width: 350,
       child: TextFormField(
           obscureText: true,
@@ -183,9 +216,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
             contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
             filled: true,
             fillColor: _color2,
-          )
-      ),
+          )),
     );
   }
 }
-
--- gdsc_server/app/controllers/users.js
+++ gdsc_server/app/controllers/users.js
@@ -3,55 +3,52 @@ const { getFirestore, collection, getDocs, doc, setDoc, getDoc} = require('fireb
 const { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut} = require("firebase/auth");
 const { ref, uploadBytes } = require('firebase/storage');
 
-const signup  = async (req, res) => {
-  const {email, password, name, isOwner} = req.body;
-  createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
-  var user = userCredential.user;
-  const data = {
-    uid: user.uid,
-    downloadURL: "",
-    following: [],
-    displayName: name,
-    role: "",
-    email: email,
-    myEvents: [],
-    clubsOwned: []
+const signup = async (req, res) => {
+  try {
+    const { email, password, name, isOwner } = req.body;
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
 
-  }
-  if(isOwner == "true"){
-    data.role = "owner"
-  }
-  else{
-    data.role = "user"
-  }
-  setDoc(doc(db, "users", data.uid), data).then(()=>{
-       res.status(200).send(JSON.stringify({ message: user}))
+    const data = {
+      uid: user.uid,
+      downloadURL: "",
+      following: [],
+      displayName: name,
+      role: isOwner === true || isOwner === 'true' ? 'owner' : 'user',
+      email: email,
+      myEvents: [],
+      clubsOwned: []
+    };
 
-     })
+    await setDoc(doc(db, "users", data.uid), data);
 
-    }).catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        res.status(500).send(JSON.stringify({ message: error}))
+    res.status(200).send(JSON.stringify({ message: 'User created successfully' }));
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    res.status(500).send(JSON.stringify({ error: { code: errorCode, message: errorMessage } }));
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+
+    res.status(200).send(JSON.stringify({ message: 'Login successful', user }));
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    res.status(500).send({
+      error: {
+        status: errorCode || 500,
+        message: errorMessage || 'Internal Server Error',
+      },
     });
-}
-const login  = async (req, res) => {
-     const {email, password} = req.body;
-  signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
-  // Signed in
-  var user = userCredential.user;
-   res.status(200).send(JSON.stringify({ message: user}))
-    }).catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        res.status(500).send({
-          error: {
-            status: errorCode || 500,
-            message: errorMessage || 'Internal Server Error',
-          },
-    });
-    });
-}
+  }
+};
+
 
 const signedIn  = async (req, res) => {
   auth.onAuthStateChanged(function(user) {
