28c0bc34a40838770b7c6e5950b34cd878af0082
Anirudh Krishna <kris.anirudh@gmail.com>
2023-12-21 23:05:39 -0800

    small change

--- gdsc_app/lib/screens/profile.dart
+++ gdsc_app/lib/screens/profile.dart
@@ -162,7 +162,7 @@ class _ProfileScreenState extends State<ProfileScreen> with TickerProviderStateM
             itemCount: user!.eventData.length,
             itemBuilder: (context, index){
               //I currently put isOwner true as temporary, change it afterwards
-              return EventCardWidget(event: user!.eventData[index], isOwner: true,);
+              return EventCardWidget(event: user!.eventData[index], isOwner: false);
             }
         ),
         ListView.builder(
