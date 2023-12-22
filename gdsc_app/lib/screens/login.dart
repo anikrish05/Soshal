543aba6269ddf9183edfb41089ddacebf1673333
Anirudh Krishna <kris.anirudh@gmail.com>
2023-12-22 11:12:09 -0800

    login verification

--- gdsc_app/lib/screens/login.dart
+++ gdsc_app/lib/screens/login.dart
@@ -15,126 +15,148 @@ class _LoginScreenState extends State<LoginScreen> {
   User user = User();
   Color _color1 = Color(0xFFFF8050);
   Color _color2 = Color(0xFFF0F0F0);
-  @override
+  String errorMessage = ""; // Added for displaying error messages
 
   void initState() {
     super.initState();
     isUserSignedIn();
   }
+
   void isUserSignedIn() async {
     print("in");
-    user.isUserSignedIn().then((check){
+    user.isUserSignedIn().then((check) {
       print(check);
-      if(check){
+      if (check) {
         Navigator.pushNamed(context, '/home');
       }
     });
   }
-  void login() async {
-    print("HIIHIHIHI");
-    user.signIn(emailController.text, passWordController.text).then((check){
-      if(check){
-        print("hi");
-        Navigator.pushNamed(context, '/home');
-      }
-    });
-  }
-  void signUpRoute(){
-    Navigator.pushNamed(context, '/sign');
 
+  Future<void> login() async {
+    print("in SignIn");
+    final response = await post(
+      Uri.parse('http://10.0.2.2:3000/api/users/login'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        "email": emailController.text,
+        "password": passWordController.text,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      Navigator.pushNamed(context, '/home');
+    } else {
+      setState(() {
+        errorMessage = "Login failed. Please check your credentials.";
+      });
+    }
+  }
+
+  void signUpRoute() {
+    Navigator.pushNamed(context, '/sign');
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-        body: Center(
-          child: ListView(
-            shrinkWrap: true,
-            children: <Widget>[
-              Image.asset('assets/image.png',
-                height: 100
-              ),
-              Padding(
-                  padding: EdgeInsets.only(bottom: 90)
-              ),
-              TextButton(
-                onPressed: () {signUpRoute(); },
-                child: Center(
-                  child: Text("create a new account",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                          fontFamily: "borel",
-                          color: Colors.black,
-                          fontSize: 18.3,
-                      )
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            Image.asset('assets/image.png', height: 100),
+            Padding(padding: EdgeInsets.only(bottom: 90)),
+            TextButton(
+              onPressed: () {
+                signUpRoute();
+              },
+              child: Center(
+                child: Text(
+                  "create a new account",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: "borel",
+                    color: Colors.black,
+                    fontSize: 18.3,
                   ),
                 ),
               ),
-              buildEmailField(),
-              Padding(
-                  padding: EdgeInsets.only(bottom: 8)
+            ),
+            buildEmailField(),
+            Padding(padding: EdgeInsets.only(bottom: 8)),
+            buildPasswordField(),
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.black,
               ),
-              buildPasswordField(),
-              TextButton(
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.black,
-                ),
-                onPressed: () {login(); },
-                child: Text('Log in',
-                    style: TextStyle(
-                        fontFamily: "borel",
-                      decoration: TextDecoration.underline,
-                      fontSize: 18.3
-                    )
+              onPressed: () {
+                login();
+              },
+              child: Text(
+                'Log in',
+                style: TextStyle(
+                  fontFamily: "borel",
+                  decoration: TextDecoration.underline,
+                  fontSize: 18.3,
                 ),
               ),
+            ),
 
-
-            ],
-          ),
-        )
+            // Display error message if there is one
+            errorMessage.isNotEmpty
+                ? Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                errorMessage,
+                style: TextStyle(color: Colors.red),
+                textAlign: TextAlign.center,
+              ),
+            )
+                : SizedBox.shrink(),
+          ],
+        ),
+      ),
     );
   }
-  Widget buildEmailField(){
+
+  Widget buildEmailField() {
     return SizedBox(
       width: 350,
       child: TextFormField(
-          controller: emailController,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(
-              borderSide: BorderSide.none,
-              borderRadius: BorderRadius.circular(50.0),
-            ),
-            hintText: "Email",
-            contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-            filled: true,
-            fillColor: _color2,
-          )
+        controller: emailController,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(50.0),
+          ),
+          hintText: "Email",
+          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          filled: true,
+          fillColor: _color2,
+        ),
       ),
     );
   }
 
-  Widget buildPasswordField(){
-    return  SizedBox(
+  Widget buildPasswordField() {
+    return SizedBox(
       width: 350,
       child: TextFormField(
-          obscureText: true,
-          controller: passWordController,
-          obscuringCharacter: '*',
-          decoration: InputDecoration(
-            border: OutlineInputBorder(
-              borderSide: BorderSide.none,
-              borderRadius: BorderRadius.circular(50.0),
-            ),
-            hintText: "Password",
-            contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-            filled: true,
-            fillColor: _color2,
-          )
+        obscureText: true,
+        controller: passWordController,
+        obscuringCharacter: '*',
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(50.0),
+          ),
+          hintText: "Password",
+          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          filled: true,
+          fillColor: _color2,
+        ),
       ),
     );
   }
-
 }
-
-
