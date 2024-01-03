e0338c8bb06151220aa5323019be7dd627076f87
AdithyaPradeep05 <adithyaapradeep@gmail.com>
2024-01-02 20:08:16 -0800

    will fix the issue

--- gdsc_app/lib/widgets/profileWidgets/otherProfileWidget.dart
+++ gdsc_app/lib/widgets/profileWidgets/otherProfileWidget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../classes/User.dart';
 import '../../classes/userData.dart';
 
 class OtherProfileWidget extends StatefulWidget {
@@ -12,17 +13,27 @@ class OtherProfileWidget extends StatefulWidget {
 class _OtherProfileWidgetState extends State<OtherProfileWidget> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        children: [
-          CircleAvatar(
-            radius: 20,
-            backgroundImage: NetworkImage(widget.user.downloadURL),
-          ),
-          SizedBox(width: 8),
-          Text(widget.user.displayName),
-        ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            for (User user in widget.user.users)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 20,
+                      backgroundImage: NetworkImage(user.downloadURL),
+                    ),
+                    SizedBox(height: 8),
+                    Text(user.displayName),
+                  ],
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
