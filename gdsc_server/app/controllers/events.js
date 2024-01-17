8e173a322ef41e68f3ffa47da6f545a117b7f1a5
Anirudh Krishna <kris.anirudh@gmail.com>
2024-01-17 01:49:30 -0800

    server changes

--- gdsc_server/app/controllers/events.js
+++ gdsc_server/app/controllers/events.js
@@ -1,7 +1,7 @@
 const { db, auth } = require('../../db/config');
 const { checkAuthorization } = require('./authorizationUtil');
 
-const { getFirestore, collection, getDocs, doc, setDoc, getDoc, addDoc } = require('firebase/firestore');
+const { getFirestore, collection, getDocs, doc, setDoc, getDoc, addDoc, deleteDoc } = require('firebase/firestore');
 const { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut, verifyIdToken } = require("firebase/auth");
 
 async function associatedClubEventAdd(clubList, eventId) {
@@ -35,11 +35,12 @@ const createEvent = async (req, res) => {
             downloadURL: downloadURL,
             latitude: latitude,
             longitude: longitude,
-            rating: 0,
             timestamp: timestamp,
             repeat: repeat,
             rsvpList: [],
-            tags: tags
+            tags: tags,
+            likedBy: [],
+            disLikedBy; []
         };
         addDoc(collection(db, "events"), data)
             .then((docRef) => {
@@ -98,8 +99,19 @@ const getEvent = async (req, res) => {
     }
 };
 
+const deleteEvent = async (req, res) => {
+    if (await checkAuthorization(req, res)) {
+        const { eventID } = req.body;
+
+        await deleteDoc(doc(db, "events". eventID));
+
+
+    }
+}
+
 module.exports = {
     createEvent,
     getFeedPosts,
-    getEvent
+    getEvent,
+    deleteEvent
 };
--- gdsc_server/app/controllers/friendGroup.js
+++ gdsc_server/app/controllers/friendGroup.js
@@ -3,8 +3,54 @@ const { getFirestore, collection, getDocs, doc, setDoc, getDoc, addDoc, updateDo
 const { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } = require("firebase/auth");
 const { uploadString, getDownloadURL, getStorage  } = require("firebase/storage");
 const { checkAuthorization } = require('./authorizationUtil');
+const axios = require('axios');
+const serverKey = 'AAAAMEF6-KY:APA91bG2-CzCuAQCFvCbXx6G2YloxVaKAkVV3xCs-dcLYMJ2PKz91S-c1_KBkqAQtlMgAIbyV9v1Z7zTG3L04nV7lw7_Q-uipZs23SNTqBUN6c2Vhqkbl9V4zPRcvCm2evFKwDwxJKwH';
+const fcmEndpoint = 'https://fcm.googleapis.com/fcm/send';
+async function addUserToGroup(userId, friendId) {
+  const docSnap = await getDoc(doc(db, "users", userId));
 
+  data = {
+    friendGroups: [...docSnap.data().friendGroups, friendId]
+  }
+  await updateDoc(doc(db, "users", userId), data)
+  return(docSnap.data().notifToken)
+}
+
+const createFriendGroup = async (req, res) => {
+  if (await checkAuthorization(req, res)) {
+    const { friendCircle, name } = req.body;
+    var devices = [];
+    const data = {
+      friendCircle: friendCircle,
+      name: name,
+    }
+    const friendCollection = collection(db, 'friends');
+    addDoc(friendCollection, data).then((docRef) => {
+      for (var i = 0; i < friendCircle.length; i++) {
+        devices.push(await addUserToGroup(friendCircle[i], docRef.id))
+      }
+      const notificationPayload = {
+        registration_ids: devices,
+        notification: {
+          title: 'Added to Friend Group',
+          body: 'You Have Been Added to the Friend Group, '+name,
+        },
+       };
+
+    const response = await axios.post(fcmEndpoint, notificationPayload, {
+      headers: {
+        Authorization: `key=${serverKey}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+      res.status(200).send(JSON.stringify({ message: "Friend Group Created" }))
+    }).catch(error => {
+      res.status(500).send(JSON.stringify({ message: "Failed" }))
+    });
+  }
+}
 
 module.exports = {
-
+  createFriendGroup
 };
