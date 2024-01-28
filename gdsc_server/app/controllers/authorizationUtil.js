f6401f91d7a9bea3001c89cf40408e3ee8c96076
Anirudh Krishna <kris.anirudh@gmail.com>
2024-01-28 14:41:17 -0800

    tokrns

--- gdsc_server/app/controllers/authorizationUtil.js
+++ gdsc_server/app/controllers/authorizationUtil.js
@@ -2,14 +2,18 @@ const { db, auth } = require('../../db/config')
 
 const checkAuthorization = async (req, res) => {
   const idToken = req.headers['authorization'];
+  console.log(req.headers.referer)
+  console.log(idToken)
   if(idToken!=null){
   try {
     // Get the current user's ID token from Firebase
     const userToken = await auth.currentUser.getIdToken();
-
+    console.log(idToken)
+    console.log(userToken)
     // Compare the user's ID token with the ID token provided in the request's authorization header
     if (userToken !== idToken) {
       // If the tokens are different, the request is unauthorized
+      console.log("token mismatch")
       res.status(401).send(JSON.stringify({ error: 'Unauthorized' }));
       return false; // Return false to indicate unauthorized
     }
@@ -17,7 +21,7 @@ const checkAuthorization = async (req, res) => {
     // If the tokens are the same, the user is authorized
     return true; // Return true to indicate authorized
   } catch (error) {
-    console.error('Error checking authorization:', error);
+    console.log('Error checking authorization:', error);
     res.status(500).send(JSON.stringify({ error: 'Internal Server Error' }));
     return false; // Return false in case of an error
   }
