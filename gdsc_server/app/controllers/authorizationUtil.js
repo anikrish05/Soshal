6531baa6cbe40e9e0a9f73dc994c0e0436ca04bb
Anirudh Krishna <kris.anirudh@gmail.com>
2024-01-28 17:43:31 -0800

    check auth changes

--- gdsc_server/app/controllers/authorizationUtil.js
+++ gdsc_server/app/controllers/authorizationUtil.js
@@ -1,32 +1,14 @@
 const { db, auth } = require('../../db/config')
 
 const checkAuthorization = async (req, res) => {
-  const idToken = req.headers['authorization'];
-  if(idToken!=null){
-  try {
-    // Get the current user's ID token from Firebase
-    const userToken = await auth.currentUser.getIdToken();
-    console.log(idToken)
-    console.log(userToken)
-    // Compare the user's ID token with the ID token provided in the request's authorization header
-    if (userToken != idToken) {
-      // If the tokens are different, the request is unauthorized
-      console.log("token mismatch")
-      res.status(401).send(JSON.stringify({ error: 'Unauthorized' }));
-      return false; // Return false to indicate unauthorized
-    }
-
-    // If the tokens are the same, the user is authorized
-    return true; // Return true to indicate authorized
-  } catch (error) {
-    console.log('Error checking authorization:', error);
-    res.status(500).send(JSON.stringify({ error: 'Internal Server Error' }));
-    return false; // Return false in case of an error
-  }
-}
-else{
-    res.status(401).send(JSON.stringify({ error: 'Unauthorized' }));
-}
+  auth.verifyIdToken(idToken)
+  .then((decodedToken) => {
+    return true
+  })
+  .catch((error) => {
+    return false
+    // Handle error
+  });
 };
 
 module.exports = { checkAuthorization };
